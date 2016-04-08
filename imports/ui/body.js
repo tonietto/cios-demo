ffbc1990ad8181c43e9f036bdbab6756199b4fbc
Gustavo Tonietto <tonietto@gmail.com>
2016-04-08 18:02:09 -0300

    Passo 3 - fixtures

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,24 +2,32 @@ import {
     Template
 } from 'meteor/templating';
 
+import {
+    Contacts
+} from '../api/contacts.js';
+
 import './body.html';
 
 Template.body.helpers( {
-    contacts: [
-        {
-            name: 'Pierre Nanterme',
-            job: 'Chairman and CEO',
-            company: 'Accenture'
-    },
-        {
-            name: 'Evan G. Greenberg',
-            job: 'CEO',
-            company: 'ACE Limited'
-    },
-        {
-            name: 'Kumar Birla',
-            job: 'Chairman',
-            company: 'Aditya Birla Group'
-    },
-  ],
+    contacts() {
+        return Contacts.find();
+    }
 } );
+
+if ( Contacts.find().count() === 0 ) {
+    Contacts.insert( {
+        name: 'Pierre Nanterme',
+        job: 'Chairman and CEO',
+        company: 'Accenture'
+    } );
+    Contacts.insert( {
+        name: 'Kumar Birla',
+        job: 'Chairman',
+        company: 'Aditya Birla Group'
+    } );
+    Contacts.insert( {
+        name: 'Evan G. Greenberg',
+        job: 'CEO',
+        company: 'ACE Limited'
+    } );
+}
